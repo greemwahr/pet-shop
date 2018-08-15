077c7aae2d9f509fa17eb826460edd3ad597852d
shadowsyntax <tibetianwolf@gmail.com>
2018-08-15 12:32:32 +0100

    Resolving merge conflicts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,4 @@ var Adoption = artifacts.require('Adoption')
 
 module.exports = function(deployer) {
   deployer.deploy(Adoption)
-<<<<<<< 539fb52d3fe3764b715d6e2b5eaa35a60a4b2b3f
 }
-=======
-}
->>>>>>> Stashing branch
